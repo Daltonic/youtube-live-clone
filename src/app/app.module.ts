d317edc24082839484b7ce8cdfc71fe94181f4a0
Daltonic <darlingtongospel@gmail.com>
2021-04-03 13:38:03 +0100

    adding the sidebar and streams components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EventsComponent } from './events/events.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { StreamsComponent } from './components/streams/streams.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AngularMaterialModule],
+  declarations: [
+    AppComponent,
+    EventsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    StreamsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
