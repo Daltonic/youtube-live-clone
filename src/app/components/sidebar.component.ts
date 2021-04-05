b4c3891729672fc7e9057e16b3bd9fc0d56c2e15
Daltonic <darlingtongospel@gmail.com>
2021-04-05 09:59:21 +0100

    styled the sidebar and imported mat-icons

--- src/app/components/sidebar.component.ts
+++ src/app/components/sidebar.component.ts
@@ -7,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
       <app-rows selected="true" title="Home" icon="home"></app-rows>
       <app-rows title="Trending" icon="whatshot"></app-rows>
       <app-rows title="Subscription" icon="subscriptions"></app-rows>
+      <hr>
+      <app-rows title="Library" icon="video_library"></app-rows>
+      <app-rows title="History" icon="history"></app-rows>
+      <app-rows title="Your Videos" icon="tv"></app-rows>
+      <app-rows title="Watch Later" icon="watch_ater"></app-rows>
+      <app-rows title="Liked Vedios" icon="thumb_up_alt_outlined"></app-rows>
+      <app-rows title="Show More" icon="expand_more_outlined"></app-rows>
+      <hr>
     </div>
   `,
   styles: [
     `
       .sidebar {
         flex: 0.2;
+      }
+
+      .sidebar > hr {
+        height: 1px;
+        border: 0;
+        background-color: lightgray;
+        margin-top: 10px;
+        margin-bttom: 10px;
       }
     `,
   ],
