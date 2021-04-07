d58eb115a1c4f7406598bca03ed4f282247c263b
Daltonic <darlingtongospel@gmail.com>
2021-04-07 14:51:41 +0100

    restructures apps structure

--- src/app/components/sidebar.component.ts
+++ src/app/components/sidebar.component.ts
@@ -3,27 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   template: `
-    <div class="sidebar">
-      <app-rows selected="true" title="Home" icon="home"></app-rows>
-      <app-rows title="Trending" icon="whatshot"></app-rows>
-      <app-rows title="Subscription" icon="subscriptions"></app-rows>
-      <hr>
-      <app-rows title="Library" icon="video_library"></app-rows>
-      <app-rows title="History" icon="history"></app-rows>
-      <app-rows title="Your Videos" icon="tv"></app-rows>
-      <app-rows title="Watch Later" icon="watch_ater"></app-rows>
-      <app-rows title="Liked Vedios" icon="thumb_up_alt_outlined"></app-rows>
-      <app-rows title="Show More" icon="expand_more_outlined"></app-rows>
-      <hr>
-    </div>
+    <app-rows selected="true" title="Home" icon="home"></app-rows>
+    <app-rows title="Trending" icon="whatshot"></app-rows>
+    <app-rows title="Subscription" icon="subscriptions"></app-rows>
+    <hr class="sidebar__hr" />
+    <app-rows title="Library" icon="video_library"></app-rows>
+    <app-rows title="History" icon="history"></app-rows>
+    <app-rows title="Your Videos" icon="tv"></app-rows>
+    <app-rows title="Watch Later" icon="watch_ater"></app-rows>
+    <app-rows title="Liked Vedios" icon="thumb_up_alt_outlined"></app-rows>
+    <app-rows title="Show More" icon="expand_more_outlined"></app-rows>
+    <hr class="sidebar__hr" />
   `,
   styles: [
     `
-      .sidebar {
-        flex: 0.2;
-      }
-
-      .sidebar > hr {
+      .sidebar__hr {
         height: 1px;
         border: 0;
         background-color: lightgray;
--- src/app/components/streams.component.ts
+++ src/app/components/streams.component.ts
@@ -2,18 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-streams',
-  template: `
-    <div class="streams">
-      <h4>Hi I'm streaming</h4>
-    </div>
-  `,
-  styles: [
-    `
-      .streams {
-        flex: 0.8;
-      }
-    `,
-  ],
+  template: ` <h4>Videos</h4> `,
+  styles: [``],
 })
 export class StreamsComponent implements OnInit {
   constructor() {}
