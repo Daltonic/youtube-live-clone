9630916dea610dd1b9e543d3dc07528d7396a808
Daltonic <darlingtongospel@gmail.com>
2021-04-13 15:04:29 +0100

    adds the chat app on the event page

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -7,12 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
+  message: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
     const tag = document.createElement('script');
     tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollToEnd()
+  }
+
+  scrollToEnd(): void {
+    const elmnt = document.getElementById('messages-container');
+    elmnt.scrollTop = elmnt.scrollHeight;
+  }
+
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
+
+  isMaxed(): Boolean {
+    return this.message.split(" ").length >= 200;
+  }
+
+  loggy(): void {
+    console.log(this.message);
+    
   }
 
 }
