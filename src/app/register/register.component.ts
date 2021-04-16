0ac51a485868ed1a59355cb03da34bf94df27344
Daltonic <darlingtongospel@gmail.com>
2021-04-16 19:36:01 +0100

    installs firebase auth to registration component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  constructor(private auth: AngularFireAuth, private route: Router) {}
 
-  constructor() { }
+  public submit(form: any): void {
+    const fullname = form.fullname;
+    const email = form.email;
+    const password = form.password;
 
-  ngOnInit(): void {
+    this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((res) => {
+        res.user
+          .updateProfile({
+            displayName: fullname,
+          })
+          .then(() => {
+            this.route.navigate(['login']);
+          });
+      })
+      .catch((error) => console.log(error));
   }
-
 }
