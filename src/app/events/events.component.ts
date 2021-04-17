62077e9323939c2736c25b736c9435760b8107c0
Daltonic <darlingtongospel@gmail.com>
2021-04-17 15:11:14 +0100

    lists events

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
 })
 export class EventsComponent implements OnInit {
-  about: boolean = false
+  about: boolean = false;
+  events: Array<any> = [];
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.listEvents();
   }
 
+  public listEvents() {
+    return this.firestore
+      .collection('events')
+      .snapshotChanges()
+      .subscribe((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          const key: string = childSnapshot.payload.doc.id;
+          const data: any = childSnapshot.payload.doc.data();
+          this.events.push({ ...data, key });
+        });
+      });
+  }
+
+  public getVideoId(url: string) {
+    const regExp = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+    return url.match(regExp)[1];
+  }
 }
