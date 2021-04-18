c78ddace45ed56da745d1cf9f627b485548bab37
Daltonic <darlingtongospel@gmail.com>
2021-04-18 20:56:42 +0100

    installs the cometchat SDK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,16 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBhD-kOhhSL-uVQ1Vp6B4eMl_xPQgQoSnc",
-    authDomain: "clone-6fd6f.firebaseapp.com",
-    databaseURL: "https://clone-6fd6f-default-rtdb.firebaseio.com",
-    projectId: "clone-6fd6f",
-    storageBucket: "clone-6fd6f.appspot.com",
-    messagingSenderId: "924185543469",
-    appId: "1:924185543469:web:22c69d1cc8484d0968c9a4",
-    measurementId: "G-M8Y3HZV2W5"
-  }
+    apiKey: 'AIzaSyBhD-kOhhSL-uVQ1Vp6B4eMl_xPQgQoSnc',
+    authDomain: 'clone-6fd6f.firebaseapp.com',
+    databaseURL: 'https://clone-6fd6f-default-rtdb.firebaseio.com',
+    projectId: 'clone-6fd6f',
+    storageBucket: 'clone-6fd6f.appspot.com',
+    messagingSenderId: '924185543469',
+    appId: '1:924185543469:web:22c69d1cc8484d0968c9a4',
+    measurementId: 'G-M8Y3HZV2W5',
+  },
+  APP_ID: '316457e59bf4759',
+  APP_KEY: '43698e155e5860980aca801deed35faf41b3c1a9',
+  APP_REGION: 'us',
 };
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { CometChat } from '@cometchat-pro/chat';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const appID = environment.APP_ID;
+const region = environment.APP_REGION;
+const appSetting = new CometChat.AppSettingsBuilder()
+  .subscribePresenceForAllUsers()
+  .setRegion(region)
+  .build();
+CometChat.init(appID, appSetting).then(
+  () => {
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err));
+    console.log('Initialization completed successfully');
+  },
+  (error) => {
+    console.log('Initialization failed with error:', error);
+    // Check the reason for error and take appropriate action.
+  }
+);
